abf4f87580bd2c0ff2340df203c25a0fdffe7bb8
Henrique Souza de Santana <64788576+318Henrique@users.noreply.github.com>
2020-06-01 10:39:21 -0300

    Initial commit

---
+++ valida.js
@@ -0,0 +1,45 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* global google */
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var target = document.querySelector('#map');
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        var latitude   = position.coords.latitude;
+        var longitude  = position.coords.longitude;
+        var coordinate = new google.maps.LatLng(latitude, longitude);
+
+        var optionsMap = {
+                    center : coordinate,
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(target, optionsMap);
+
+        var configMarker = {
+                             position : coordinate,
+                             map : map,
+                             title: "Você Está Aqui!"
+                            };
+
+        var marker = new google.maps.Marker(configMarker);
+
+    });
+});
+
+
+
+
