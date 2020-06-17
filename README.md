949e827f96d95a92515d79466c11552e9b2875fa
Henrique Souza de Santana <64788576+318Henrique@users.noreply.github.com>
2020-06-16 23:13:29 -0300

    nova estruturação do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReciEco
- 
--- src/scripts/getlocation.js
+++ src/scripts/getlocation.js
@@ -1,18 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* global google */
-
 document.addEventListener('DOMContentLoaded', function(){
-
     var target = document.querySelector('#map');
     
     navigator.geolocation.getCurrentPosition(function(position) {
