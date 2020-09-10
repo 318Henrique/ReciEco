898fefaf3383651a76e722e12fb7620552f28412
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-09-10 11:15:27 -0400

    structure of the page done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import Header from '../Components/Header';
 import Api from '../Api/api';
 import Message from '../Components/Message';
@@ -6,10 +6,11 @@ import { AuthContext } from '../Context/Auth';
 
 export default function Profile(){
     const [ dataProfile, handleDataProfile ] = useState({});
+    const [ residuesProfile, handleResiduesProfile ] = useState([]);
     const [ message, newMessage ] = useState(null);
     const { userDetail: { dataUser } } = useContext(AuthContext);
 
-    useEffect(() => {
+    const getDataPersonaProfile = useCallback(() => {
         (async () => {
             try {
                 const response = await Api.get('/profile/i/');
@@ -22,6 +23,25 @@ export default function Profile(){
         })()
     }, [])
 
+    const getResidueesPersonProfile = useCallback(() => {
+        (async () => {
+            try {
+                const response = await Api.get('/profile/residues/');
+                const { content } = response.data;
+
+                handleResiduesProfile(content)
+            } catch (error) {
+                newMessage({ content: error })
+            }
+        })()
+    }, [])
+
+    useEffect(() => {
+        getDataPersonaProfile();
+        getResidueesPersonProfile();
+
+    }, [getDataPersonaProfile, getResidueesPersonProfile])
+
     return(
         <>
         <Header/>
@@ -33,24 +53,40 @@ export default function Profile(){
                 <div className='igp'>{ dataUser.name }</div>
             </section>
             <section className='section-contact'>
-                <h2 className='title-section'>Contato</h2>
-                <div>
-                    <span>{dataProfile.mail}</span>
-                    <span><a href={`https://api.whatsapp.com/send?phone=${dataProfile.whatsapp}&text=Ola`}>{dataProfile.whatsapp}</a></span>
+                <div className='section-contact-title'>
+                    <h2 className='title-section'>Contato</h2>
+                </div>
+                <div className='section-contact-content'>
+                    <div>
+                        <span>{dataProfile.mail}</span>
+                        <span><a href={`https://api.whatsapp.com/send?phone=${dataProfile.whatsapp}&text=Ola`}>{dataProfile.whatsapp}</a></span>
+                    </div>
                 </div>
             </section>
-            <section className='section-pattern-profile'>
+            <section className='section-address'>
                 <h2 className='title-section'>Endereço</h2>
-                <div>
-                    <span>{dataProfile.address} - {dataProfile.address_number}</span>
-                    <span>Bairro {dataProfile.neghborhood}</span>
+                <div className='content'>
+                    <span>{dataProfile.address} - {dataProfile.address_number}, {dataProfile.neghborhood}</span>
                     <span>{dataProfile.city} - {dataProfile.state}</span>
-                    <span>Coordenadas {dataProfile.coord_lat}, {dataProfile.coord_lng}</span>
+                    <span>Latitude {dataProfile.coord_lat}, Longitude {dataProfile.coord_lng}</span>
                 </div>
             </section>
 
-            <section className='section-pattern-profile'>
+            <section className='section-residues'>
                 <h2 className='title-section'>Meus Resíduos</h2>
+                <div className='control choise-rediues'>
+                    {
+                        residuesProfile.map(({ id, residues_name, icon }) => (
+                            <div
+                                key={ id }
+                                className="choise-residues-item"
+                            >
+                                <img src={ icon } alt=''/>
+                                <span> { residues_name } </span>
+                            </div>
+                        ))
+                    }
+                </div>
             </section>
         </div>
         <Message message={message}/>
