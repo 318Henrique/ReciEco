af646d3613ec059521518fe662b620f1b78fd108
Henrique Souza de Santana <64788576+318Henrique@users.noreply.github.com>
2022-08-09 17:14:58 -0400

    Update Registration.js

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -104,7 +104,7 @@ export default function Registration(){
                 </div>
                 
                 <div className='control'>
-                    <div>
+                    <div className='carrosel-fotoperfil'>
                     <div className='boxField file-input'>
                         <label htmlFor='foto'>
                             <img src={imagePreview} alt=''/>
@@ -114,7 +114,7 @@ export default function Registration(){
                         }}/>
                         
                     </div>
-                    <h4 className='carrosel-fotoperfil'>Foto de Perfil</h4>
+                    <h4>Foto de Perfil</h4>
                     </div>
                     <div className='boxField'>
                         <label htmlFor='person_name'>Nome Completo <span>*</span></label>
